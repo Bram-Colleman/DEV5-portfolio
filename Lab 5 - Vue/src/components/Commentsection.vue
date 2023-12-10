30e996a9b449520728bd18ab61dfc8a71d3beb17
Bram-Colleman <r0820751@student.thomasmore.be>
2023-12-10 13:21:42 +0100

    feat: add send comment

--- Lab 5 - Vue/src/components/Commentsection.vue
+++ Lab 5 - Vue/src/components/Commentsection.vue
@@ -2,6 +2,8 @@
 import { ref, onMounted } from 'vue';
 
 const comments = ref([]);
+const comment = ref("");
+const username = ref("");
 onMounted(() => {
     fetchComments();
 });
@@ -13,25 +15,50 @@ const fetchComments = async () => {
     res.data.messages.forEach(message => {
         comments.value.push(message);
     });
-    console.log(comments.value);
 };
 
-const send = () => {
-    
-}
+const send = async () => {
+    try {
+        if (comment.value && username.value) {
+            const response = await fetch('https://dev5-lab4-1hlz.onrender.com/api/v1/messages', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    message: {
+                        user: username.value,
+                        text: comment.value ,
+                    }
+                }),
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            } else {
+                const data = await response.json();
+-               comments.value.push(data.data.message);
+                comment.value = '';
+            }
+        }
+    }
+    catch (error) {
+        console.error('An error occurred while posting the comment:', error);
+    }
+};
 
 </script>
 <template>
-<ul>
-    <li v-for="comment in comments">
-        <p><strong>{{ comment.user }}</strong></p>
-        <p>{{ comment.text }}</p>
-    </li>
-</ul>
-<div class="add">
-    <input type="text" name="" id="">
-    <button id="send" @click="send">Send</button>
-</div>
+    <ul>
+        <li v-for="comment in comments.slice().reverse()">
+            <p><strong>{{ comment.user }}</strong></p>
+            <p>{{ comment.text }}</p>
+        </li>
+    </ul>
+    <div class="add">
+        <input type="text" v-model="username" placeholder="Username" id="input--user">
+        <input type="text" v-model="comment" placeholder="Add comment..." id="input--text">
+        <button id="send" v-on:click="send">Send</button>
+    </div>
 </template>
 <style scoped>
 ul {
@@ -47,8 +74,11 @@ ul {
     display: flex;
     flex-grow: 1;
 }
-input {
-    width: 45vw;
+#input--user {
+    width: 10vw;
+}
+#input--text {
+    width: 35vw;
 }
 button {
     width: 5vw;
